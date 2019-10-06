d3c881ba4bb5e64e239db226598ad27ba7bf1dfa
Bani <bani@Banis-MacBook-Pro.local>
2019-10-05 20:12:50 -0400

    Updates

---
+++ precache-manifest.dd7ef7e013dfafa80c66f7b148cb4bb2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "04968073760aeee25e7da962defb2158",
+    "url": "/search-api-in-react/index.html"
+  },
+  {
+    "revision": "99d5fcef3b7f808dc938",
+    "url": "/search-api-in-react/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "bfa320bb6c3e220b6891",
+    "url": "/search-api-in-react/static/css/main.80ac6327.chunk.css"
+  },
+  {
+    "revision": "99d5fcef3b7f808dc938",
+    "url": "/search-api-in-react/static/js/2.87b21f55.chunk.js"
+  },
+  {
+    "revision": "bfa320bb6c3e220b6891",
+    "url": "/search-api-in-react/static/js/main.ff2d6254.chunk.js"
+  },
+  {
+    "revision": "ab1c394e411ba17a581b",
+    "url": "/search-api-in-react/static/js/runtime-main.88438ef3.js"
+  },
+  {
+    "revision": "fe12bd9f6939dd9c8ec7ebc48ea5635f",
+    "url": "/search-api-in-react/static/media/background.fe12bd9f.jpg"
+  }
+]);
